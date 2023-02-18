4b5b4825d399e545a319ceb9311a6fce2fa684f6
Muhammad Naayif <66357485+naayifmuhammad@users.noreply.github.com>
2023-02-18 20:09:45 +0530

    fixed .\mergeTwoSortedArrays.c

--- mergeTwoSortedArrays.c
+++ mergeTwoSortedArrays.c
@@ -2,21 +2,23 @@
 void main()
 {
     int a1[50], a2[50], a3[100], i, j, k = 0, n, m;
-    printf("Enter the size of 1st array");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
+    printf("Enter the size of 1st array: ");
+    scanf("%d", &m);
+    printf("Enter elements for the first array:\n");
+    for (i = 0; i < m; i++)
     {
         scanf("%d", &a1[i]);
     }
-    printf("Enter the size of 2nd array");
-    scanf("%d", &m);
-    for (j = 0; j < m; j++)
+    printf("Enter the size of 2nd array: ");
+    scanf("%d", &n);
+    printf("Enter elements for the second array:\n");
+    for (j = 0; j < n; j++)
     {
         scanf("%d", &a2[j]);
     }
     i = 0;
     j = 0;
-    while (i < n && j < m)
+    while (i < m && j < n)
     {
         if (a1[i] < a2[j])
         {
@@ -30,27 +32,28 @@ void main()
         }
         k++;
     }
-    if (i >= n)
+    if (i >= m)
     {
-        while (j < m)
+        while (j < n)
         {
             a3[k] = a2[j];
             j++;
             k++;
         }
     }
-    if (j >= m)
+    if (j >= n)
     {
-        while (i < n)
+        while (i < m)
         {
             a3[k] = a1[i];
             i++;
             k++;
         }
     }
+
     printf("merged array:\n");
-    for (i = 0; i < n + m; i++)
+    for (i = 0; i < (m + n); i++)
     {
-        printf("%d", a3[i]);
+        printf("%d  ", a3[i]);
     }
 }
