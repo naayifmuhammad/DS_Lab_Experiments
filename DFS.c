366d05bae56eddef52bba12908a1d5bb4bbd7487
Muhammad Naayif <66357485+naayifmuhammad@users.noreply.github.com>
2023-02-19 11:13:03 +0530

    added starting vertex request to DFS

--- DFS.c
+++ DFS.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
 void DFS(int);
-int G[10][10], visited[10], n; // n is no of vertices and graph is sorted in array G[10][10]
+int G[10][10], visited[10], n, startingVertex;
 void main()
 {
     int i, j;
     printf("Enter number of vertices:");
 
     scanf("%d", &n);
-    // read the adjacency matrix
     printf("\nEnter adjacency matrix of the graph:");
 
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
             scanf("%d", &G[i][j]);
-    for (i = 0; i < n; i++)
+    for (i = 1; i <= n; i++)
         visited[i] = 0;
-    DFS(0);
+    printf("\nEnter a starting vertex");
+    scanf("%d", &startingVertex);
+    DFS(startingVertex);
 }
 void DFS(int i)
 {
     int j;
     printf("\n%d", i);
     visited[i] = 1;
-    for (j = 0; j < n; j++)
+    for (j = 1; j <= n; j++)
         if (!visited[j] && G[i][j] == 1)
             DFS(j);
 }
